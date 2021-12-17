466177d58f7e8fd206df32cc56dd76898ba65b59
Marc <07marcmepri@gmail.com>
2021-12-17 20:36:15 +0100

    commit

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const keys = require('./config/keys/keys.json')
-const uri = keys.URI
+const uri = keys.URI || process.env.URI
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
 .then(db => console.log('[DB] Se ha conectado a mongodb'))
 .catch(err => console.log(`[DB] Ha ocurrido un error: ${err}`))
