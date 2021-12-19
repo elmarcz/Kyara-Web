b4671f3147cf8514f227feede19481fa7808f848
Marc <07marcmepri@gmail.com>
2021-12-19 14:12:32 +0100

    make it better

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const keys = require('./config/keys/keys.json')
-const uri = keys.URI || process.env.URI
+const uri = process.env.URI || keys.URI
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
